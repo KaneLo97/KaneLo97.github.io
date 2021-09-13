3c998377b9a82d081f4ffffa72f0d5ee95751849
klohogti <klohogti@sfu.ca>
2021-09-12 17:56:16 -0700

    added invalid character validation for hangman

--- js/hangman.js
+++ js/hangman.js
@@ -4,8 +4,9 @@ let incorrectGuess = 0;
 let correctGuess = 0;
 let isCorrectGuess = false;
 let guessedCharacterList = "";
-let maxGuesses = 20;
-let maxIncorrectGuesses = 8;
+
+const MAX_GUESSES = 20;
+const MAX_INCORRECT_GUESSES = 8;
 
 $(document).ready(function() {
     let index = Math.floor(Math.random() * (WORDLIST.length));
@@ -22,9 +23,9 @@ $(document).ready(function() {
 });
 
 $("#guess").on("input", function() {
-    guessEntered = $(this).val();
+    guessEntered = $(this).val().toLowerCase();
     let isCorrectGuess = false;
-    if (guessEntered !== '' && !isCharactersAlreadyEntered(guessEntered)) { // if the player is not deleting a character.
+    if (guessEntered !== '' && !isCharactersAlreadyEntered(guessEntered) && isValidGuess(guessEntered)) { // if the player is not deleting a character.
         if (guessedCharacterList.length === 0) { //player has not yet make any guesses
             guessedCharacterList += guessEntered;
         } else if (guessedCharacterList.indexOf(guessEntered) == -1) { // player has entered a character -> add it to the character list
@@ -60,13 +61,28 @@ function isCharactersAlreadyEntered(guessEntered) {
 
     // show this alert each time the player enters a guess which he already enters
     $("#alreadyEntered").html(`<div class='alert alert-warning'><em>You already entered this guess: ${guessEntered}.</em></div><br>`);
+    // hide the invalid message;
+    $("#invalidCharacter").html("");
+    return true;
+}
+
+// check if the character is a letter
+function isValidGuess(guessEntered) {
+    // only characters which are not letters will be equal to each other
+    if (guessEntered.toLowerCase() === guessEntered.toUpperCase()) {
+        $("#invalidCharacter").html("<div class='alert alert-danger'>Invalid Guess. Please enter letter only.</div><br>");
+        return false;
+    }
+
+    // hide the invalid message;
+    $("#invalidCharacter").html("");
     return true;
 }
 
 function determineGameStatus() {
-    if (totalGuesses > maxGuesses) {
+    if (totalGuesses > MAX_GUESSES) {
        setGameStatus("<div class='alert alert-danger'><h4 class='text-center'>Game Lost</h4><p class='text-center'>You have exceeded the maximum number of total guesses allowed.<br>The word to be guessed was <q><b><i>" + randomWord + "</i></b></q>.</p></div><br>");
-    } else if (incorrectGuess > maxIncorrectGuesses) {
+    } else if (incorrectGuess > MAX_INCORRECT_GUESSES) {
        setGameStatus("<div class='alert alert-danger'><h4 class='text-center'>Game Lost</h4><p class='text-center'>You have exceeded the maximum number of incorrect guesses allowed.<br>The word to be guessed was <q><b><i>" + randomWord + "</i></b></q>.</p></div><br>");
     } else if (correctGuess === randomWord.length) {
         setGameStatus("<div class='alert alert-success'><h4 class='text-center'>Congrats!! Game Won</h4><p class='text-center'>You have correctly guessed the word <q><b><i>" + randomWord + "</i></b></q></div><br>");
